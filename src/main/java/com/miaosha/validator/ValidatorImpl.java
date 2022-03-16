9ccfac166674b72e9fad3c9cc87680f9e11f1eb8
Aspetto343 <1760906445@qq.com>
2022-03-16 22:21:41 +0800

    增加校验规则

---
+++ src/main/java/com/miaosha/validator/ValidatorImpl.java
@@ -0,0 +1,14 @@
+package com.miaosha.validator;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidatorImpl implements InitializingBean {
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
