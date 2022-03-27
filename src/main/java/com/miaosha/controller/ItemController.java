bc2242025562e19cfa49d14eeac477668c8e9cab
Aspetto343 <1760906445@qq.com>
2022-03-27 08:41:02 +0800

    新增商品实现

---
+++ src/main/java/com/miaosha/controller/ItemController.java
@@ -0,0 +1,53 @@
+package com.miaosha.controller;
+
+import com.miaosha.controller.viewobject.ItemVO;
+import com.miaosha.error.BusinessException;
+import com.miaosha.response.CommonReturnType;
+import com.miaosha.service.ItemService;
+import com.miaosha.service.model.ItemModel;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+
+@Controller("/item")
+@RequestMapping("/item")
+@CrossOrigin(allowCredentials = "true",allowedHeaders = "*")
+public class ItemController extends BaseController{
+
+    @Resource
+    private ItemService itemService;
+    //创建商品的controller
+    public CommonReturnType createItem(@RequestParam(name = "title")String title,
+                                       @RequestParam(name = "description")String description,
+                                       @RequestParam(name = "price")BigDecimal price,
+                                       @RequestParam(name = "stock")Integer stock,
+                                       @RequestParam(name = "imgUrl")String imgUrl) throws BusinessException {
+        //封装service请求用来创建商品
+        ItemModel itemModel = new ItemModel();
+        itemModel.setTitle(title);
+        itemModel.setDescription(description);
+        itemModel.setPrice(price);
+        itemModel.setStock(stock);
+        itemModel.setImgUrl(imgUrl);
+
+        ItemModel item = itemService.createItem(itemModel);
+        ItemVO itemVO = convertVOFromModel(item);
+
+        return CommonReturnType.create(itemVO);
+    }
+
+    private ItemVO convertVOFromModel(ItemModel itemModel){
+        if(itemModel == null){
+            return null;
+        }
+        ItemVO itemVO = new ItemVO();
+        BeanUtils.copyProperties(itemModel,itemVO);
+
+        return itemVO;
+    }
+}
---
+++ src/main/java/com/miaosha/service/impl/ItemServiceImpl.java
@@ -0,0 +1,99 @@
+package com.miaosha.service.impl;
+
+import com.miaosha.dao.ItemDOMapper;
+import com.miaosha.dao.ItemStockDOMapper;
+import com.miaosha.dataobject.ItemDO;
+import com.miaosha.dataobject.ItemStockDO;
+import com.miaosha.error.BusinessException;
+import com.miaosha.error.EnumBusinessError;
+import com.miaosha.service.ItemService;
+import com.miaosha.service.model.ItemModel;
+import com.miaosha.validator.ValidationResult;
+import com.miaosha.validator.ValidatorImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ItemServiceImpl implements ItemService {
+    @Resource
+    private ValidatorImpl validator;
+
+    @Resource
+    private ItemDOMapper itemDOMapper;
+
+    @Resource
+    private ItemStockDOMapper itemStockDOMapper;
+
+    private ItemDO convertItemDOFromItemModel(ItemModel itemModel){
+        if(itemModel == null){
+            return null;
+        }
+        ItemDO itemDO = new ItemDO();
+        BeanUtils.copyProperties(itemModel,itemDO);
+        itemDO.setPrice(itemModel.getPrice().doubleValue());
+
+        return itemDO;
+    }
+
+    private ItemStockDO convertItemStockDOFromItemModel(ItemModel itemModel){
+        if(itemModel == null){
+            return null;
+        }
+        ItemStockDO itemStockDO = new ItemStockDO();
+        itemStockDO.setItemId(itemModel.getId());
+        itemStockDO.setStock(itemModel.getStock());
+        return itemStockDO;
+    }
+    @Override
+    @Transactional
+    public ItemModel createItem(ItemModel itemModel) throws BusinessException {
+        //校验入参
+        ValidationResult validate = validator.validate(itemModel);
+        if(validate.isHasErrors()){
+            throw new BusinessException(EnumBusinessError.PARAMETER_VALIDATION_ERROR,validate.getErrMsg());
+        }
+
+        //转化ItemModel->dataobject
+        ItemDO itemDO = convertItemDOFromItemModel(itemModel);
+
+        //写入数据库
+        itemDOMapper.insertSelective(itemDO);
+        itemModel.setId(itemDO.getId());
+        ItemStockDO itemStockDO = convertItemStockDOFromItemModel(itemModel);
+        itemStockDOMapper.insertSelective(itemStockDO);
+
+        //返回创建完成的对象
+        return itemModel;
+    }
+
+    @Override
+    public List<ItemModel> listItem() {
+        return null;
+    }
+
+    @Override
+    public ItemModel getItemById(Integer id) {
+        ItemDO itemDO = itemDOMapper.selectByPrimaryKey(id);
+        if(itemDO == null){
+            return null;
+        }
+        //操作获得库存数量
+        ItemStockDO itemStockDO = itemStockDOMapper.selectByItemId(itemDO.getId());
+
+        //将dataobject->model
+        ItemModel itemModel = convertModelFromDataObject(itemDO, itemStockDO);
+
+        return itemModel;
+    }
+
+    private ItemModel convertModelFromDataObject(ItemDO itemDO,ItemStockDO itemStockDO){
+        ItemModel itemModel = new ItemModel();
+        BeanUtils.copyProperties(itemDO,itemModel);
+        itemModel.setPrice(new BigDecimal(itemDO.getPrice()));
+        itemModel.setStock(itemStockDO.getStock());
+        return itemModel;
+    }
+}
