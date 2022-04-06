7ef2a66f159191d21f98de7cad0b23694ea53b88
Aspetto343 <1760906445@qq.com>
2022-04-06 17:15:29 +0800

    增加生成订单和生成订单交易号方法

--- src/main/java/com/miaosha/service/impl/OrderServiceImpl.java
+++ src/main/java/com/miaosha/service/impl/OrderServiceImpl.java
@@ -1,5 +1,9 @@
 package com.miaosha.service.impl;
 
+import com.miaosha.dao.OrderDOMapper;
+import com.miaosha.dao.SequenceDOMapper;
+import com.miaosha.dataobject.OrderDO;
+import com.miaosha.dataobject.SequenceDO;
 import com.miaosha.error.BusinessException;
 import com.miaosha.error.EnumBusinessError;
 import com.miaosha.service.ItemService;
@@ -8,10 +12,16 @@ import com.miaosha.service.UserService;
 import com.miaosha.service.model.ItemModel;
 import com.miaosha.service.model.OrderModel;
 import com.miaosha.service.model.UserModel;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -21,6 +31,12 @@ public class OrderServiceImpl implements OrderService {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private OrderDOMapper orderDOMapper;
+
+    @Resource
+    private SequenceDOMapper sequenceDOMapper;
 
     @Override
     @Transactional
@@ -39,11 +55,68 @@ public class OrderServiceImpl implements OrderService {
         if(amount <= 0 || amount > 99){
             throw new BusinessException(EnumBusinessError.PARAMETER_VALIDATION_ERROR,"数量信息不正确");
         }
+
         //2.落单减库存
+        boolean result = itemService.decreaseStock(itemId,amount);
+        if(result){
+            throw new BusinessException(EnumBusinessError.STOCK_NOT_ENOUGH);
+        }
 
         //3.订单入库
+        OrderModel orderModel = new OrderModel();
+
+        orderModel.setUserId(userId);
+        orderModel.setItemId(itemId);
+        orderModel.setAmount(amount);
+        orderModel.setItemPrice(itemById.getPrice());
+        orderModel.setOrderPrice(itemById.getPrice().multiply(new BigDecimal(amount)));
+
+        //生成交易流水号，订单号
+        orderModel.setId(generateOrderNo());
+        OrderDO orderDO = convertFromOrderModel(orderModel);
+        orderDOMapper.insertSelective(orderDO);
 
         //4.返回前端
-        return null;
+        return orderModel;
+    }
+
+    //即使其他方法事务失败回滚，调用该方法仍然会成功执行
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    String generateOrderNo(){
+        //订单号有16位
+        StringBuilder stringBuilder = new StringBuilder();
+
+        //前8位为时间信息，年月日
+        LocalDateTime now = LocalDateTime.now();
+        String nowDate = now.format(DateTimeFormatter.ISO_DATE).replace("-","");
+        stringBuilder.append(nowDate);
+
+        //中间6位为自增序列
+        //获取当前sequence
+        int sequence = 0;
+        SequenceDO order_info = sequenceDOMapper.getSequenceByName("order_info");
+        sequence = order_info.getCurrentValue();
+        order_info.setCurrentValue(order_info.getCurrentValue()+order_info.getStep());
+        sequenceDOMapper.updateByPrimaryKeySelective(order_info);
+        String sequenceStr = String.valueOf(sequence);
+        for(int i = 0;i < 6-sequenceStr.length();i++){
+            stringBuilder.append(0);
+        }
+        stringBuilder.append(sequenceStr);
+
+        //最后2位为分库分表位
+        stringBuilder.append("00");
+
+        return stringBuilder.toString();
+    }
+
+    private OrderDO convertFromOrderModel(OrderModel orderModel){
+        if(orderModel == null){
+            return null;
+        }
+
+        OrderDO orderDO = new OrderDO();
+        BeanUtils.copyProperties(orderModel,orderDO);
+        return orderDO;
     }
 }
