f2e205366907a7d0d85495259befc92e110c07d8
Aspetto343 <1760906445@qq.com>
2022-04-08 18:41:38 +0800

    增加dao层文件

---
+++ src/main/java/com/miaosha/dao/ItemDOMapper.java
@@ -0,0 +1,24 @@
+package com.miaosha.dao;
+
+import com.miaosha.dataobject.ItemDO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ItemDOMapper {
+    List<ItemDO> listItem();
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ItemDO record);
+
+    int insertSelective(ItemDO record);
+
+    ItemDO selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ItemDO record);
+
+    int updateByPrimaryKey(ItemDO record);
+
+    int increaseSales(@Param("id")Integer id,@Param("amount")Integer amount);
+}
---
+++ src/main/java/com/miaosha/dataobject/PromoDO.java
@@ -0,0 +1,68 @@
+package com.miaosha.dataobject;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class PromoDO implements Serializable {
+    private Integer id;
+
+    private String promoName;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private Integer itemId;
+
+    private Double promoItemPrice;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPromoName() {
+        return promoName;
+    }
+
+    public void setPromoName(String promoName) {
+        this.promoName = promoName == null ? null : promoName.trim();
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Double getPromoItemPrice() {
+        return promoItemPrice;
+    }
+
+    public void setPromoItemPrice(Double promoItemPrice) {
+        this.promoItemPrice = promoItemPrice;
+    }
+}
